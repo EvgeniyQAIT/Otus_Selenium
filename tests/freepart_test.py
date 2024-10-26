f135589292c1110ed13d29dc63253ef3e217f33c
Evgeniy_Nikitin <Evgeniy.qait@yandex.ru>
2024-10-26 17:37:22 +0300

    Домашняя работа "Простые автотесты"

--- tests/freepart_test.py
+++ tests/freepart_test.py
@@ -3,43 +3,46 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
+
 def test_login(browser):
     browser.get("http://192.168.0.161:8081/en-gb?route=account/login")
-    browser.find_element(By.ID, "input-email").send_keys("Evgeniy.qait@gmail.com") #нашли элемент логин и ввели логин
-    browser.find_element(By.ID, "input-password").send_keys("qwerty123") #нашли элемент пароль и ввели пароль
-    browser.find_element(By.XPATH, '//*[@id="form-login"]/div[3]/button').click() #нашли кнопку и нажали
-    WebDriverWait(browser, timeout=2).until(EC.url_contains("customer_token")) #подождали пока URL поменяется и там в URL будет токен
-    assert "My Account" in browser.title #проверили заголовок страницы
+    browser.find_element(By.ID, "input-email").send_keys("Evgeniy.qait@gmail.com")
+    browser.find_element(By.ID, "input-password").send_keys("qwerty123")
+    browser.find_element(By.XPATH, '//*[@id="form-login"]/div[3]/button').click()
+    WebDriverWait(browser, timeout=2).\
+        until(EC.url_contains("customer_token"))
+    assert "My Account" in browser.title
 
 def test_add_card(browser):
     browser.get("http://192.168.0.161:8081/en-gb?route=common/home")
-    browser.maximize_window() #сделали максимальный размер окна
-    button = browser.find_element(By.XPATH, '//*[@id="content"]/div[2]/div[2]/div/div[2]/form/div/button[1]') #нашли кнопку добавления в корзину
+    button1 = browser.find_element(By.XPATH, '//*[@id="content"]/div[2]/div[2]/div/div[2]/form/div/button[1]')
     actions = ActionChains(browser)
-    actions.move_to_element(button).perform() #пролистали до кнопки
-    button.click() #добавили в корзину кликнув
-    button2 = browser.find_element(By.XPATH, '//*[@id="header-cart"]/div/button') #нашли кнопку корзины
-    actions = ActionChains(browser)
-    actions.move_to_element(button2).perform() #пролистали до кнопки
-    button2.click() #кликнули в корзину
-# при выполнение теста test_add_card вылезает ошибка и я не пойму как её победить, он не видит элемент
+    actions.move_to_element(button1).perform()
+    button1.click()
+    WebDriverWait(browser, timeout=8).\
+        until(EC.visibility_of_element_located((By.XPATH, '//*[@id="alert"]/div')))
+    WebDriverWait(browser, timeout=3).\
+        until(EC.visibility_of_element_located((By.XPATH, '//*[@id="alert"]/div/button'))).click()
+    browser.find_element(By.XPATH, '//*[@id="header-cart"]/div/button').click()
+    assert 'iPhone' in browser.find_element(By.XPATH, '//*[@id="header-cart"]/div/ul/li/table/tbody/tr/td[2]/a').text
+
 
 
 def test_currency_home(browser):
     browser.get("http://192.168.0.161:8081/en-gb?route=common/home")
-    button1 = browser.find_element(By.XPATH, '//*[@id="form-currency"]/div/a/span') #нашли элемент переключения валюты
-    button1.click() #кликнули
-    button2 = browser.find_element(By.XPATH, '//*[@id="form-currency"]/div/ul/li[1]/a') #нашли элемент евро
-    button2.click() #переключили валюту кликнув на элемент
-#надо как-то проверить что текст с $ сменился на € на главной странице у цены товаров
+    browser.find_element(By.XPATH, '//*[@id="form-currency"]/div/a/span').click()
+    browser.find_element(By.XPATH, '//*[@id="form-currency"]/div/ul/li[1]/a').click()
+    assert '€' in browser.find_element(By.CLASS_NAME, 'price-new').text
 
 
 
-# def test_currency_tablets(browser):
-#     browser.get("http://192.168.0.161:8081/en-gb/catalog/tablet")
-#     button1 = browser.find_element(By.XPATH, '//*[@id="form-currency"]/div/a/span')
-#     button1.click()
-#     button2 = browser.find_element(By.XPATH, '//*[@id="form-currency"]/div/ul/li[1]/a')
-#     button2.click()
-#
-#1
+
+
+def test_currency_tablets(browser):
+    browser.get("http://192.168.0.161:8081/en-gb/catalog/tablet")
+    browser.find_element(By.XPATH, '//*[@id="form-currency"]/div/a/span').click()
+    browser.find_element(By.XPATH, '//*[@id="form-currency"]/div/ul/li[1]/a').click()
+    assert '€' in browser.find_element(By.CLASS_NAME, 'price-new').text
+
+
+
--- tests/twopart_test.py
+++ tests/twopart_test.py
@@ -1,22 +1,20 @@
 
-def test_check_title(browser): #тест на проверку заголовка главной страницы
+def test_check_title(browser):
     browser.get("http://192.168.0.161:8081/")
     assert "Your Store" in browser.title, "Тайтл страницы отличается от ожидаемого"
 
-def test_check_title_catalog(browser): #тест на проверку заголовка карточек товара
+def test_check_title_catalog(browser):
     browser.get("http://192.168.0.161:8081/en-gb/catalog/tablet")
     assert "Tablet" in browser.title, "Тайтл страницы отличается от ожидаемого"
 
-def test_check_title_prodcut_card_iphone(browser): #тест на проверку заголовка карточки айфона
+def test_check_title_prodcut_card_iphone(browser):
     browser.get("http://192.168.0.161:8081/en-gb/product/iphone")
     assert "iPhone" in browser.title, "Тайтл страницы отличается от ожидаемого"
 
-def test_check_title_registration_user(browser): #тест на проверку заголовка регистрации пользователя
+def test_check_title_registration_user(browser):
     browser.get("http://192.168.0.161:8081/en-gb?route=account/register")
     assert "Register Account" in browser.title, "Тайтл страницы отличается от ожидаемого"
 
-def test_check_title_administration(browser): #тест на проверку заголовка страницы администатора
+def test_check_title_administration(browser):
     browser.get("http://192.168.0.161:8081/administration/")
     assert "Administration" in browser.title, "Тайтл страницы отличается от ожидаемого"
-
-#1
